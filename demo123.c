e65fbbebf30f9865f3eb1cdcbfc961eb6423ff16
RutujaCh <57203998+RutujaCh@users.noreply.github.com>
2019-10-31 08:35:12 +0530

    inputs from user

--- demo123.c
+++ demo123.c
@@ -2,8 +2,11 @@
 int main()
 {
   int a,b,c,big;
-
-  a=20;b=10;c=3;
+  printf("enter three numbers");
+  scanf("%d %d %d",&a,&b,&c);
+  //a=20;b=10;c=3;
   big=(a>b)?(a>c?a:c):(b>c?b:c);
+  printf("biggest num is %d",big);
   return 0;
 }
+
