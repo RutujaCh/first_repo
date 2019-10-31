282b4e491dade8e94471740dbb2707668997baef
RutujaCh <57203998+RutujaCh@users.noreply.github.com>
2019-10-31 09:06:14 +0530

    using conditional statement

--- demo123.c
+++ demo123.c
@@ -2,11 +2,23 @@
 int main()
 {
   int a,b,c,big;
-  printf("enter three numbers");
-  scanf("%d %d %d",&a,&b,&c);
-  //a=20;b=10;c=3;
-  big=(a>b)?(a>c?a:c):(b>c?b:c);
-  printf("biggest num is %d",big);
+printf("enter three numbers");
+scanf("%d %d %d",&a,&b,&c);
+//a=20;b=10;c=3;
+//big=(a>b)?(a>c?a:c):(b>c?b:c);
+//using conditional statement
+if(a>b)
+{
+  if(a>c)
+    big=a;
+  else
+    big=c;
+}
+else if (b>c)
+  big=b;
+else 
+  big=c; 
+printf("biggest num is %d",big);
   return 0;
 }
 
